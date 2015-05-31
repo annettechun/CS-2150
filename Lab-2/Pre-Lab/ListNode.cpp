59514c0787643be34d5b9f1d255519e9e1aeb709
Annette Chun <amc4sq@virginia.edu>
2015-05-31 02:17:36 -0400

    Create ListNode.cpp

---
+++ Lab-2/Pre-Lab/ListNode.cpp
@@ -0,0 +1,14 @@
+//Annette Chun
+//amc4sq
+//01/26/2015
+//ListNode.cpp
+
+#include "ListNode.h"
+using namespace std;
+
+//default constructor
+ListNode::ListNode(){
+  value = 0;
+  next = NULL; //for doubly linked lists
+  previous = NULL; //for doubly linked lists
+}
