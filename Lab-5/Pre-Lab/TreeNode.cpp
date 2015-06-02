127266b0cd8270e87c6d5da4fb9f9ac520c03c2c
Annette Chun <amc4sq@virginia.edu>
2015-06-02 01:03:01 -0400

    Create TreeNode.cpp

---
+++ Lab-5/Pre-Lab/TreeNode.cpp
@@ -0,0 +1,23 @@
+//Annette Chun
+//amc4sq
+//02/23/15
+//TreeNode.cpp
+
+// TreeNode.cpp:  Tree Node method implementations
+// CS 2150: Lab 5
+
+#include "TreeNode.h"
+
+//Default Constructor -left and right are NULL, value '?'
+TreeNode::TreeNode() {
+    value="?";
+    left=NULL;
+    right=NULL;
+}
+
+//Constructor - sets value to val
+TreeNode::TreeNode(const string & val) {
+    value=val;
+    left=NULL;
+    right=NULL;
+}
